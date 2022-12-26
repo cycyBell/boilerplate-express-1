d25713c324763c0105bda1d2013e13a20ba6552d
cycyBell <cycydjelassem@gmail.com>
2022-12-26 04:58:19 +0100

    Succesfull

--- myApp.js
+++ myApp.js
@@ -1,5 +1,18 @@
 let express = require('express');
 let app = express();
+require('dotenv').config();
+app.get('/', function(req, res){
+    res.sendFile(__dirname + "/views/index.html");
+  });
+app.use("/public",express.static(__dirname + '/public'));
+app.get('/json', function(req, res){
+    if (process.env.MESSAGE_STYLE === uppercase) {
+        res.json({"message": "HELLO JSON"});
+    } else {
+        res.json({"message": "Hello json"});
+    }
+    
+});
 
 
 
